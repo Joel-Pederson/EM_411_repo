dca93be77fe78e0304df8718c2c461aac5092fb6
Joel Pederson <157438776+Joel-Pederson@users.noreply.github.com>
2025-10-26 15:26:03 -0400

    Wireframe Task 2 Design options
    Lay out structure for defining Task 2 design options. Waiting to hear thoughts from team on potential designs

--- OS_4/Run_Task_2.m
+++ OS_4/Run_Task_2.m
@@ -1,17 +1,37 @@
 %% -- EM.411 OS 4 Task 2 -- %%
+% This script models 5 unique fleet architectures and generates the
+% data table required for the Task 2 deliverable
+clear all;
 
+%% -- Load Database -- %%
 [roadDB, bikeDB] = load_DB();
 
-% -- 2. Define a Test Architecture --
-% 'design' must be a struct containing the index for EACH component.
-% Let's pick a simple "all 1s" architecture for our first test:
-test_design.chassis = 1;         % C1 (2 pax)
-test_design.battery_pack = 1;  % P1 (50 kWh)
-test_design.battery_charger = 1; % G1 (10 kW)
-test_design.motor = 1;           % M1 (50 kW)
-test_design.autonomy = 2;        % A4 (Level 4) - Let's pick 2 for variety
+%% -- 2. Define Fleet-Level Model Assumptions -- %%
 
-[Road_EV_Design, cost, isValid] = calculateRoadVehicle(test_design, roadDB);
-[Bike_EV_Design, cost, isValid] = calculateBikeVehicle(test_design, bikeDB);
+
+%% -- 3. Define Unique Architectures -- %%
+
+designs = {};     % Stores the component indices for each arch
+archTypes = {};   % Stores the type: 'road' or 'bike'
+fleetSizes = [];  % Stores the fleet size assumption for each arch
+
+% Option 1 - Ultra Minimal Road Vehicle 
+% 'design' must be a struct containing the index for EACH component.
+design{1}.chassis = 1;         % C1 (2 pax)
+design{1}.battery_pack = 1;    % P1 (50 kWh)
+design{1}.battery_charger = 1; % G1 (10 kW)
+design{1}.motor = 1;           % M1 (50 kW)
+design{1}.autonomy = 2;        % A4 (Level 4)
+
+% Option 2 - TBD
+
+% Option 3 - All Bike Fleet
+
+% Option 4 - TBD
+
+% Option 5 - TBD
+
+[Road_EV_Design, cost, isValid] = calculateRoadVehicle(design, roadDB);
+[Bike_EV_Design, cost, isValid] = calculateBikeVehicle(design, bikeDB);
 
 % EV_design.total_fleet_cost = 
