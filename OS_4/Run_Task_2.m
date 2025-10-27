7a6f44dbca24df03fca979a0e8629f43aad327ab
Joel Pederson <157438776+Joel-Pederson@users.noreply.github.com>
2025-10-27 10:29:24 -0400

    additional bug fixes and error handling refinement

--- OS_4/Run_Task_2.m
+++ OS_4/Run_Task_2.m
@@ -30,8 +30,8 @@ designs{1}.battery_pack = 1;    % P1 (50 kWh)
 designs{1}.battery_charger = 1; % G1 (10 kW)
 designs{1}.motor = 1;           % M1 (50 kW)
 designs{1}.autonomy = 2;        % A4 (Level 4)
-designs.archTypes{1} = 'road';
-designs.fleetSizes(1) = 25; % Our assumption for fleet size
+designs{1}.archTypes{1} = 'road';
+designs{1}.fleetSizes(1) = 25; % Our assumption for fleet size
 
 % Option 2 - TBD
 
@@ -42,29 +42,28 @@ designs.fleetSizes(1) = 25; % Our assumption for fleet size
 % % Option 5 - Mixed Fleet (Road + Bike)
 % designs{5}.road = designs{1}; % Use the TBD design
 % designs{5}.bike = designs{4}; % Use the TBD design
-% designs.archTypes{5} = 'mixed';
-% designs.fleetSizes{5}.road = 15; % Assumption: 15 road vehicles
-% designs.fleetSizes{5}.bike = 50; % Assumption: 50 bikes
+% designs{5}.archTypes = 'mixed';
+% designs{5}.fleetSizes.road = 15; % Assumption: 15 road vehicles
+% designs{5}.fleetSizes.bike = 50; % Assumption: 50 bikes
 
 %% -- Execute Model -- %%
 for ii = 1:length(designs)
     design = designs{1};
 
-    if design.archTypes == 'road'
+    if strcmp(design.archTypes, 'road')
         [Road_EV_Design, cost, isValid] = calculateRoadVehicle(design, roadDB);
         if ~isvalid % Check if the current state is valid; if not, skip to the next iteration
             continue
         end
         %Fleet calculations
 
-
-    elseif design.archTypes == 'bike'
+    elseif strcmp(design.archTypes, 'bike')
         [Bike_EV_Design, cost, isValid] = calculateBikeVehicle(design, bikeDB);
         if ~isValid
             continue
         end
 
-    else %mixed fleet
+    elseif strcmp(design.archTypes, 'mixed')
         %Calculate Road component
         [Road_EV_Design, roadVehicle_cost, road_isValid] = calculateRoadVehicle(design, roadDB);
         %Calculate Bike component
@@ -74,8 +73,10 @@ for ii = 1:length(designs)
             isValid = 0;
             continue
         end
+    else % Error handling for incompatible or undefined architecture type - alert user
+        str = sprintf('Design %d does not have a compatable or defined archType. Must be road, bike, or mixed.',ii);
+        error(str)
     end
-
 end %end model execution
 
 % EV_design.total_fleet_cost =
